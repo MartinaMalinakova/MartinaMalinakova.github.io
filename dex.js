207d4df78d1c81a4cd3dd84ff921c7abd6f48591
MartinaMalinakova <martina695@gmail.com>
2022-05-21 21:48:10 +0400

    Moralis DEX

--- dex.js
+++ dex.js
@@ -20,14 +20,22 @@
 //login logout
 //add from here down
 async function login() {
-    let user = Moralis.User.current();
-    if (!user) {
-         user = await Moralis.authenticate();
-       }
-    console.log("logged in user:", user);
-    getStats();
+   let user = Moralis.User.current();
+   if (!user) {
+     user = await Moralis.authenticate();
    }
-    
+   console.log("logged in user:", user);
+ }
+
+async function logOut() {
+   await Moralis.User.logOut();
+   console.log("logged out");
+ }
+
+ document.getElementById("btn-login").onclick = login;
+ document.getElementById("btn-logout").onclick = logOut;
+
+//initialize swap    
 async function initSwapForm(event) {
       event.preventDefault();
        $selectedToken.innerText = event.target.dataset.symbol;
